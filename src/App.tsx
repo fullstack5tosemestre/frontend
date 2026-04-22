d108d3fa3add550ca3d436e6d5a69f9756560ce5
dainz <mbenjaminarancibia@gmail.com>
2026-04-22 15:19:46 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import "../src/css/styles.css";
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    );
+}
+
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className="container">
+            <h1>Oal mudno</h1>
+            <button className="btn btn-primary">test</button>
+        </div>
+    );
+}
+
+export default Home;
