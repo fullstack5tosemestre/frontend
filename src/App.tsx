4562c8291e950cb7ad598c659ced573746dc2362
dainz <mbenjaminarancibia@gmail.com>
2026-05-12 23:34:53 -0400

    ALOSANS

--- src/App.tsx
+++ src/App.tsx
@@ -6,4 +6,28 @@ import "../src/css/styles.css";
 // Inventario
 import ProductsPage from "./pages/ProductsPage";
 import BranchesPage from "./pages/BranchesPage";
-impo
+import WarehousesPage from "./pages/WarehousesPage";
+
+// Pedidos
+import OrdersPage from "./pages/OrdersPage";
+
+// Usuarios
+import UsersPage from "./pages/UsersPage";
+import RolesPage from "./pages/RolesPage";
+
+export default function App() {
+    return (
+        <Routes>
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/productos" element={<ProductsPage />} />
+                <Route path="/sucursales" element={<BranchesPage />} />
+                <Route path="/bodegas" element={<WarehousesPage />} />
+                <Route path="/pedidos" element={<OrdersPage />} />
+                <Route path="/usuarios" element={<UsersPage />} />
+                <Route path="/roles" element={<RolesPage />} />
+                <Route path="*" element={<Home />} />
+            </Route>
+        </Routes>
+    );
+}
